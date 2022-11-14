a1f9291fc7554c20b8116a8a5349b6a66a254a04
bogda <bogdanhalici1989@gmail.com>
2022-11-14 17:10:14 +0000

    2st commit

--- src/Menu.java
+++ src/Menu.java
@@ -1,30 +1,35 @@
 import java.util.Scanner;
-
 public class Menu {
 
     Phonebook phonebook = new Phonebook();
 
-    void showMenu(){
-        System.out.println("Phonebook menu");
+    void showMenu() {
+        System.out.println(">>>>>> Phonebook menu <<<<<<");
         System.out.println("1 - Show phonebook");
         System.out.println("2 - Add contact");
         System.out.println("3 - Search contact");
         System.out.println("4 - Delete contact");
         Scanner scanner = new Scanner(System.in);
         int userInput = scanner.nextInt();
-        if(userInput == 1){
+        if (userInput == 1) {
             phonebook.showPhonebook();
             showMenu();
         }
-        if(userInput == 2){
+        if (userInput == 2) {
             System.out.println("Add contact details");
             phonebook.readAndAddContacts();
             showMenu();
         }
-        if(userInput ==3){
-            System.out.println("Search name/number");
-            phonebook.readAndSearch();
+        if (userInput == 3) {
+            System.out.println("Search name/number/email in the Phone Book");
+            phonebook.searchAndShowAllContactsFound(scanner.next());
             showMenu();
+        }
+        if (userInput == 4) {
+            System.out.println("Search name/number/email that you want to delete");
+            phonebook.searchAndDeleteContact(scanner.next());
+            showMenu();
+
         }
     }
 }
--- src/Phonebook.java
+++ src/Phonebook.java
@@ -16,10 +16,15 @@ public class Phonebook {
     }
 
     void showPhonebook() {
-        System.out.println("        **Phonebook**       ");
+        System.out.println("         Phonebook        ");
         System.out.println("****************************");
+        int a = 0;
         for (int i = 0; i < MyListOfContacts.size(); i++) {
-            showContact(MyListOfContacts.get(i));
+            a++;
+            if (a >= 1) {
+                System.out.println("  Position: " + a);
+                showContact(MyListOfContacts.get(i));
+            }
         }
         System.out.println("****************************");
     }
@@ -59,56 +64,168 @@ public class Phonebook {
 
     }
 
-    void readAndSearch() {
+    void searchAndShowAllContactsFound(String nameToSearch) {
         ArrayList<Contacts> listOfContactsFound = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        String nameToSearch = scanner.nextLine();
-        int resultFound = 0;
+        int resulOfContactsFound = 0;
         for (Contacts x : MyListOfContacts) {
-            if (x.firtname.contains(nameToSearch)
-                    || x.lastname.contains(nameToSearch)
-                    || x.number.contains(nameToSearch)
-                    || x.email.contains(nameToSearch)) {
-                resultFound++;
+            if (x.firtname.contains(nameToSearch) || x.lastname.contains(nameToSearch) || x.email.contains(nameToSearch) || x.number.contains(nameToSearch)) {
                 listOfContactsFound.add(x);
+                resulOfContactsFound++;
             }
         }
-
-        if (resultFound == 0) {
+        if (resulOfContactsFound == 0) {
             System.out.println("No contacts found");
+
         }
-        if (resultFound == 1) {
-            System.out.println("Only 1 contact found: ");
-            System.out.println("****************************");
-            for (int i = 0; i < listOfContactsFound.size(); i++) {
-                showContact(listOfContactsFound.get(i));
+        if (resulOfContactsFound > 1) {
+            System.out.println("We found multiple contacts");
+            int a = 0;
+            for (Contacts x : listOfContactsFound) {
+                a++;
+                if (a >= 1) {
+                    System.out.println("  Position: " + a);
+                }
+                showContact(x);
             }
-            System.out.println("****************************");
-        } else if (resultFound > 1) {
-            System.out.println("Multiple contacts found: ");
-            System.out.println("****************************");
-            for (int h = 0; h < listOfContactsFound.size(); h++) {
-                showContact(listOfContactsFound.get(h));
+        }
+        if (resulOfContactsFound == 1) {
+            System.out.println("Only one contact found");
+            for (Contacts x : MyListOfContacts) {
+                if (x.firtname.contains(nameToSearch) || x.lastname.contains(nameToSearch) || x.email.contains(nameToSearch) || x.number.contains(nameToSearch)) {
+                    showContact(x);
+                }
             }
-            System.out.println("****************************");
         }
     }
 
-
-/*    void readAndSearch2() {
+    void searchAndDeleteContact(String nameToSearch) {
         ArrayList<Contacts> listOfContactsFound = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        String nameToSearch = scanner.nextLine();
-        int resultFound = 0;
-        for (Contacts x : MyListOfContacts) {
-            if (x.firtname.contains(nameToSearch)
-                    || x.lastname.contains(nameToSearch)
-                    || x.number.contains(nameToSearch)
-                    || x.email.contains(nameToSearch)) {
-                resultFound++;
-                listOfContactsFound.add(x);
+
+        int resulOfContactsFound = 0;
+        int count = 0;
+
+        for (int i = 0; i < MyListOfContacts.size(); i++) {
+
+            if (MyListOfContacts.get(i).firtname.contains(nameToSearch)
+                    || MyListOfContacts.get(i).lastname.contains(nameToSearch)
+                    || MyListOfContacts.get(i).email.contains(nameToSearch)
+                    || MyListOfContacts.get(i).number.contains(nameToSearch)) {
+                listOfContactsFound.add(MyListOfContacts.get(i));
+                resulOfContactsFound++;
             }
         }
-    }*/
-}
+        if (resulOfContactsFound == 0) {
+            System.out.println("No contacts found");
 
+        }
+
+        if (resulOfContactsFound > 1) {
+            System.out.println("We found multiple contacts");
+            int a = 0;
+            ArrayList<Contacts> listSaveToDelete = new ArrayList<>();
+            for (Contacts m : listOfContactsFound) {
+                a++;
+                if (m.firtname.contains(nameToSearch)
+                        || m.lastname.contains(nameToSearch)
+                        || m.number.contains(nameToSearch)
+                        || m.email.contains(nameToSearch)) {
+                    listSaveToDelete.add(m);
+                }
+                if (a >= 1) {
+                    System.out.println("  Position: " + a);
+                    showContact(m);
+                }
+            }
+            System.out.println("Select position to delete");
+            System.out.println("or type 'all' to delete all contacts found.");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.next();
+            if (input.contains("all")) {
+                for (Contacts q : listOfContactsFound) {
+                    for (Contacts s : MyListOfContacts) {
+                        if (q.firtname.contains(s.firtname)
+                                && q.lastname.contains(s.lastname)
+                                && q.number.contains(s.number)
+                                && q.email.contains(s.email)) {
+                            MyListOfContacts.remove(s);
+                            break;
+                        }
+                    }
+                }
+                System.out.println("All contacts found were deleted!");
+            }
+            // inputul nostru trebuie sa fie mai mare de 49 -> tip char si mai mic de 56 tot tip char,
+            else if (input.contains("1")) {
+                for (Contacts i : MyListOfContacts) {
+                    if (listSaveToDelete.get(0).firtname.contains(i.firtname)
+                            && listSaveToDelete.get(0).lastname.contains(i.lastname)
+                            && listSaveToDelete.get(0).number.contains(i.number)
+                            && listSaveToDelete.get(0).email.contains(i.email)) {
+                        MyListOfContacts.remove(i);
+                        count++;
+                        System.out.println("Contact with position " + count + " was deleted!");
+                        break;
+                    }
+                }
+            } else if (input.contains("2")) {
+                for (Contacts i : MyListOfContacts) {
+                    if (listSaveToDelete.get(1).firtname.contains(i.firtname)
+                            && listSaveToDelete.get(1).lastname.contains(i.lastname)
+                            && listSaveToDelete.get(1).number.contains(i.number)
+                            && listSaveToDelete.get(1).email.contains(i.email)) {
+                        MyListOfContacts.remove(i);
+                        count = count + 2;
+                        System.out.println("Contact with position " + count + " was deleted!");
+                        break;
+                    }
+                }
+
+            } else if (input.contains("3")) {
+                for (Contacts i : MyListOfContacts) {
+                    if (listSaveToDelete.get(2).firtname.contains(i.firtname)
+                            && listSaveToDelete.get(2).lastname.contains(i.lastname)
+                            && listSaveToDelete.get(2).number.contains(i.number)
+                            && listSaveToDelete.get(2).email.contains(i.email)) {
+                        MyListOfContacts.remove(i);
+                        count = count + 3;
+                        System.out.println("Contact with position " + count + " was deleted!");
+                        break;
+                    }
+                }
+            } else if (input.contains("4")) {
+                for (Contacts i : MyListOfContacts) {
+                    if (listSaveToDelete.get(3).firtname.contains(i.firtname)
+                            && listSaveToDelete.get(3).lastname.contains(i.lastname)
+                            && listSaveToDelete.get(3).number.contains(i.number)
+                            && listSaveToDelete.get(3).email.contains(i.email)) {
+                        MyListOfContacts.remove(i);
+                        count = count + 4;
+                        System.out.println("Contact with position " + count + " was deleted!");
+                        break;
+                    }
+                }
+
+            } else {
+                System.out.println("Please enter a valid position");
+
+
+            }
+        }
+        if (resulOfContactsFound == 1) {
+            System.out.println("This contact:");
+            for (int i = 0; i < MyListOfContacts.size(); i++) {
+
+                if (MyListOfContacts.get(i).firtname.contains(nameToSearch)
+                        || MyListOfContacts.get(i).lastname.contains(nameToSearch)
+                        || MyListOfContacts.get(i).email.contains(nameToSearch)
+                        || MyListOfContacts.get(i).number.contains(nameToSearch)) {
+                    showContact(MyListOfContacts.get(i));
+                    MyListOfContacts.remove(i);
+                }
+
+            }
+            System.out.println("was deleted.");
+
+        }
+    }
+}
